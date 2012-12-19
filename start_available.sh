55bda536176bdcb24858ee2417f475c6d29625e2
Panagiotis Filippidis <panagiotis.filippidis@haw-hamburg.de>
2012-12-19 15:08:25 +0100

    start on all lan hosts

---
+++ start_available.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TEAM_NO=10
+MULTICAST_IP="225.10.1.5"
+IF_NAME=eth2
+
+for i in `nmap -sP 172.16.1.2-18 | grep "appears to be up"`
+do
+  if [[ $i =~ ^[[:digit:]] ]]
+  then
+    HOST_NO=${HOSTNAME#lab}
+    ./start_all.sh $HOST_NO
+  fi
+done
