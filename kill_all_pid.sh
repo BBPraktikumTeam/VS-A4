ccf75ad96f4e2e8dc87755cc8ad8ea2cc32ab3d4
xabgesagtx <oliver.behncke@haw-hamburg.de>
2012-12-19 15:35:29 +0100

    Fixed kill_all_pid

--- kill_all_pid.sh
+++ kill_all_pid.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-ls -1 | grep [\.]pid |  while read pid_name
+for pid_name in `ls -1 | grep [\.]pid`
 do
    host=${pid_name%.pid}
+   echo "Killing: " $host
    ssh $USER@$host -o BatchMode=yes "cd $HOME/workspace/VS-A4/; kill `cat $host.pid`; rm $host.pid"
 done
