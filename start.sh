277c5abd6b5b2e29ce06f86dc63bc3758848c5e1
xabgesagtx <oliver.behncke@haw-hamburg.de>
2012-12-19 09:42:58 +0100

    Select interface in start script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# example usage: 10 10 "225.10.1.2" "192.168.17.12"
-java datasource.DataSource 10 1 | erl -sname sender -setcookie hallo -boot start_sasl -noshell -s coordinator start $1 $2 $3 $4
+# example usage: 10 10 "225.10.1.2" eth0
+IF_IP=$(/sbin/ifconfig $4 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+echo "Using IP:" $IF_IP "for interface" $4
+java datasource.DataSource $1 $2 | erl -sname sender -setcookie hallo -boot start_sasl -noshell -s coordinator start $1 $2 $3 $IF_IP
--- start_dq.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java datasource.DataSource 10 1 | erl -name sender -setcookie hallo -noshell -s dataqueue start 
