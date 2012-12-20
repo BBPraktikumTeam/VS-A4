66b63cc864bbbea62de77d531bdb65985839fac5
xabgesagtx <oliver.behncke@haw-hamburg.de>
2012-12-20 10:47:07 +0100

    Added kill java

--- kill_all_pid.sh
+++ kill_all_pid.sh
@@ -3,5 +3,5 @@ for pid_name in `ls -1 | grep [\.]pid`
 do
    host=${pid_name%.pid}
    echo "Killing: " $host
-   ssh $USER@$host -o StrictHostKeyChecking=no -o BatchMode=yes "cd $HOME/workspace/VS-A4/; kill `cat $host.pid`; rm $host.pid"
+   ssh $USER@$host -o StrictHostKeyChecking=no -o BatchMode=yes "cd $HOME/workspace/VS-A4/; kill `cat $host.pid`; rm $host.pid; killall java"
 done
