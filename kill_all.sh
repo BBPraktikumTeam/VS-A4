c64ab606c5c91aff909e9c0b0c702218dae4714f
xabgesagtx <oliver.behncke@haw-hamburg.de>
2012-12-20 11:03:25 +0100

    Piped error output to dev/null

--- kill_all.sh
+++ kill_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for host in "$@"
 do
-    ssh $USER@$host -o BatchMode=yes "cd $HOME/workspace/VS-A4/; kill `cat $host.pid`; rm $host.pid; killall java"
+    ssh $USER@$host -o BatchMode=yes "cd $HOME/workspace/VS-A4/; kill `cat $host.pid`; rm $host.pid; killall java > /dev/null/ 2>&1"
 done
